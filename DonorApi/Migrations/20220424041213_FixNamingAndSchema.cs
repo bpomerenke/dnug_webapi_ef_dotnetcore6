58d3e01f75ca775dd95eb81de0b2d6e0cc936dd5
Ben Pomerenke <bpomerenke@gmail.com>
2022-04-23 23:15:03 -0500

    using postgres naming conventions

---
+++ DonorApi/Migrations/20220424041213_FixNamingAndSchema.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DonorApi.Migrations
+{
+    public partial class FixNamingAndSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Donors",
+                table: "Donors");
+
+            migrationBuilder.EnsureSchema(
+                name: "data");
+
+            migrationBuilder.RenameTable(
+                name: "Donors",
+                newName: "donors",
+                newSchema: "data");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                schema: "data",
+                table: "donors",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                schema: "data",
+                table: "donors",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                schema: "data",
+                table: "donors",
+                newName: "phone_number");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_donors",
+                schema: "data",
+                table: "donors",
+                column: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_donors",
+                schema: "data",
+                table: "donors");
+
+            migrationBuilder.RenameTable(
+                name: "donors",
+                schema: "data",
+                newName: "Donors");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Donors",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Donors",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "phone_number",
+                table: "Donors",
+                newName: "PhoneNumber");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Donors",
+                table: "Donors",
+                column: "Id");
+        }
+    }
+}
--- DonorApi/Models/Donor.cs
+++ DonorApi/Models/Donor.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DonorApi.Models;
 
+[Table("donors", Schema = "data")]
 public class Donor
 {
+    [Key]
+    [Column("id")]
     public int Id { get; set; }
     
+    [Column("name")]
     public string Name { get; set; }
     
+    [Column("phone_number")]
     public string PhoneNumber { get; set; }
 }
