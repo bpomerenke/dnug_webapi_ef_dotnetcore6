37aa0a7a3b3f70f6aa570ed1682348576b3d4c93
Ben Pomerenke <bpomerenke@gmail.com>
2022-05-02 22:29:27 -0500

    finished contract and saving pledges

--- DonorApi/Contracts/DonorDto.cs
+++ DonorApi/Contracts/DonorDto.cs
@@ -2,9 +2,11 @@ namespace DonorApi.Contracts;
 
 public class DonorDto
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string PhoneNumber { get; set; }
     public string AdditionalInfo { get; set; }
     
     public int Result { get; set; }
+    public int Amount { get; set; }
 }
--- DonorApi/Controllers/DonorsController.cs
+++ DonorApi/Controllers/DonorsController.cs
@@ -22,11 +22,44 @@ public class DonorsController : ControllerBase
     }
 
     [HttpGet("")]
-    public IEnumerable<DonorDto> Get()
+    public IEnumerable<DonorDto> GetAll()
     {
         _logger.LogInformation("Fetching donors...");
         var donors = _donorDbContext.Donors.Include(d => d.Designations).ToList();
 
         return _mapper.Map<List<DonorDto>>(donors);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<DonorDto> Get(int id)
+    {
+        var donor = _donorDbContext.Donors.Include(x=>x.Designations).FirstOrDefault(x => x.Id == id);
+        if (donor == null)
+        {
+            return BadRequest("Invalid Donor");
+        }
+
+        return _mapper.Map<DonorDto>(donor);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<DonorDto>> Update(int id, DonorDto donorDto)
+    {
+        var donor = _donorDbContext.Donors.Include(x => x.Pledges).FirstOrDefault(x => x.Id == id);
+
+        if (donor == null)
+        {
+            return BadRequest("Invalid Donor");
+        }
+
+        if (donorDto.Result == 3)
+        {
+            donor.Pledges.Add(new Pledge{ Amount = donorDto.Amount, Date = DateTimeOffset.UtcNow});
+        }
+
+        donor.CurrentDriveStatus = donorDto.Result;
+        await _donorDbContext.SaveChangesAsync();
+
+        return _mapper.Map<DonorDto>(donor);
     }
 }
